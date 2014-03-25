565b6019bdd9c7825f144bb2aafd8ee822df4773
Gregory Bush <gbush235711@gmail.com>
2014-03-24 21:27:13 -0400

    Simplify and organize

--- telengard.elm
+++ telengard.elm
@@ -2,24 +2,60 @@
 
 import Keyboard
 
+-- Abstract Model : Rooms and the player position
+
 data Boundary = EMPTY | DOOR | WALL
 
-data Feature = NONE | ELEVATOR | PIT | TELEPORTER | STAIRS | ALTAR | FOUNTAIN | CUBE | THRONE | BOX
+data Feature = NONE | ELEVATOR | PIT | TELEPORTER | STAIRS_UP | STAIRS_DOWN
+  | STAIRS_BOTH | ALTAR | FOUNTAIN | CUBE | THRONE | BOX
 
 data Command = ERROR | NORTH | EAST | SOUTH | WEST | UP | DOWN
 
 type Room = {top : Boundary, left : Boundary, feature : Feature}
 
-type GameState = {x : Int, y : Int, z : Int}
+type Position = {x : Int, y : Int, z : Int}
 
-type Coord = Int
+type GameState = Position
 
-xo = 1.6915
-yo = 1.4278
-zo = 1.2462
+-- Control : Movement commands
 
-frac : Float -> Float
-frac r = r - toFloat (floor r)
+keyCodeCommand : Keyboard.KeyCode -> Command
+keyCodeCommand n = case n of
+  37 -> WEST
+  38 -> NORTH
+  39 -> EAST
+  40 -> SOUTH
+  68 -> DOWN
+  85 -> UP
+  _ -> ERROR
+
+commandStream : Signal Command
+commandStream = keyCodeCommand <~ Keyboard.lastPressed
+
+-- State Changes: How the player moves
+
+initState = {x = 25, y = 13, z = 1}
+
+bound : Int -> Int
+bound x = clamp 1 200 x
+
+vbound : Int -> Int
+vbound x = clamp 1 50 x
+
+update : Command -> GameState -> GameState
+update c s = case c of
+  ERROR -> s
+  NORTH -> {s | y <- bound (s.y - 1)}
+  EAST -> {s | x <- bound (s.x + 1)}
+  SOUTH -> {s | y <- bound (s.y + 1)}
+  WEST -> {s | x <- bound (s.x - 1)}
+  UP -> {s | z <- vbound (s.z - 1)}
+  DOWN -> {s | z <- vbound (s.z + 1)}
+
+stateSignal : Signal GameState
+stateSignal = foldp update initState commandStream
+
+-- Concrete Model: A procedurally generated map
 
 decodeBoundary : Int -> Boundary
 decodeBoundary x = let n = x `mod` 4 in
@@ -27,7 +63,7 @@ decodeBoundary x = let n = x `mod` 4 in
      | n == 3 -> WALL
      | otherwise -> EMPTY
 
-decodeBoundaryAt : Coord -> Int -> Boundary
+decodeBoundaryAt : Int -> Int -> Boundary
 decodeBoundaryAt c h = if c == 1 || c == 201 then WALL else decodeBoundary h
 
 decodeFeature : Int -> Feature
@@ -36,7 +72,7 @@ decodeFeature n = case n of
   1 -> ELEVATOR
   2 -> PIT
   3 -> TELEPORTER
-  4 -> STAIRS
+  4 -> STAIRS_DOWN
   5 -> ALTAR
   6 -> FOUNTAIN
   7 -> CUBE
@@ -46,42 +82,59 @@ decodeFeature n = case n of
   10 -> ELEVATOR
   11 -> PIT
   12 -> TELEPORTER
-  13 -> STAIRS
+  13 -> STAIRS_DOWN
   14 -> ALTAR
   15 -> FOUNTAIN
 
-codesAt : Coord -> Coord -> Coord -> (Int, Int)
-codesAt x y z = let
-    fx = toFloat x
-    fy = toFloat y
-    fz = toFloat z
-    q = fx * xo + fy * yo + fz * zo + (fx + xo) * (fy + yo) * (fz + zo)
-    h = floor (frac q * 4694)
-    hi = h `div` 256
-    ft = if hi == 0 || hi > 5 then 0 else floor (frac (10 * q) * 15) + 1
-  in
-    (h, ft)  
+emptyRoom = {top = EMPTY, left = EMPTY, feature = NONE}
 
-roomAt : Coord -> Coord -> Coord -> Room
-roomAt x y z = if x <= 0 || y <= 0 || x >= 202 || y >= 202 || z <= 0 || z > 50 then {top = EMPTY, left = EMPTY, feature = NONE} else
-  if x == 201 && y > 0 && y < 201 then {top = EMPTY, left = WALL, feature = NONE} else
-  if y == 201 && x > 0 && x < 201 then {top = WALL, left = EMPTY, feature = NONE} else
-  if x == 201 && y == 201 then {top = EMPTY, left = EMPTY, feature = NONE} else
+onSoutheastCorner : Position -> Bool
+onSoutheastCorner {x, y, z} = x == 201 && y == 201
+
+inBounds : Position -> Bool
+inBounds {x, y, z} = x >= 1 && x <= 201 && y >= 1 && y <= 201 && z >= 1 && z <= 50
+
+xo = 1.6915
+yo = 1.4278
+zo = 1.2462
+
+frac : Float -> Float
+frac r = r - toFloat (floor r)
+
+rawRoomAt : Position -> Room
+rawRoomAt p = if not (inBounds p) || onSoutheastCorner p then emptyRoom else
+  if p.x == 201 then {emptyRoom | left <- WALL} else
+  if p.y == 201 then {emptyRoom | top <- WALL} else
   let
-    fx = toFloat x
-    fy = toFloat y
-    fz = toFloat z
+    fx = toFloat p.x
+    fy = toFloat p.y
+    fz = toFloat p.z
     q = fx * xo + fy * yo + fz * zo + (fx + xo) * (fy + yo) * (fz + zo)
     h = floor (frac q * 4694)
     hi = h `div` 256
     ft = if hi == 0 || hi > 5 then 0 else floor (frac (10 * q) * 15) + 1
   in
-    {top = decodeBoundaryAt y h, left = decodeBoundaryAt x (h `div` 4), feature = decodeFeature ft}
+    {top = decodeBoundaryAt p.y h,
+     left = decodeBoundaryAt p.x (h `div` 4),
+     feature = decodeFeature ft}
 
-hdoor = image 56 8 "hdoor.png" |> toForm
-hwall = image 56 8 "hwall.png" |> toForm
-vdoor = image 8 56 "vdoor.png" |> toForm
-vwall = image 8 56 "vwall.png" |> toForm
+mapFeature : Feature -> Feature -> Room -> Room
+mapFeature a b r = if r.feature == a then {r | feature <- b} else r
+
+mergeFeature : Feature -> Room -> Room
+mergeFeature f r = case f of
+  STAIRS_DOWN -> case r.feature of
+    STAIRS_DOWN -> {r | feature <- STAIRS_BOTH}
+    _ -> {r | feature <- STAIRS_UP}
+  _ -> r
+
+roomAt : Position -> Room
+roomAt p = let m = mergeFeature (rawRoomAt {p | z <- p.z - 1}).feature (rawRoomAt p) in
+  if | p.z == 1 -> mapFeature ELEVATOR STAIRS_UP m
+     | p.z == 50 -> (mapFeature PIT ELEVATOR . mapFeature STAIRS_DOWN NONE) m
+     | otherwise -> m
+
+-- View: Show the map in the neighborhood of the player and a map legend
 
 stairsUp = image 24 21 "stairs.png" |> toForm
 stairsDown = image 24 21 "stairsDown.png" |> toForm
@@ -95,23 +148,29 @@ cube = image 8 8 "grayMistyCube.png" |> toForm
 throne = image 24 21 "throne.png" |> toForm
 box = image 8 8 "box.png" |> toForm
 
-featureSymbolAt : Int -> Feature -> Feature -> [Form]
-featureSymbolAt z f a = case (f, a) of
-  (STAIRS, STAIRS) -> [stairsBoth]
-  (_, STAIRS) -> [stairsUp]
-  (NONE, _) -> []
-  (ELEVATOR, _) -> if z <= 1 then [stairsUp] else [elevator]
-  (PIT, _) -> if z >= 50 then [elevator] else [pit]
-  (TELEPORTER, _) -> [teleport]
-  (STAIRS, _) -> if z >= 50 then [] else [stairsDown]
-  (ALTAR, _) -> [altar]
-  (FOUNTAIN, _) -> [fountain]
-  (CUBE, _) -> [cube]
-  (THRONE, _) -> [throne]
-  (BOX, _) -> [box]
+featureSymbol : Feature -> [Form]
+featureSymbol f = case f of
+  NONE -> []
+  STAIRS_UP -> [stairsUp]
+  STAIRS_BOTH -> [stairsBoth]
+  STAIRS_DOWN -> [stairsDown]
+  ELEVATOR -> [elevator]
+  PIT -> [pit]
+  TELEPORTER -> [teleport]
+  STAIRS_DOWN -> [stairsDown]
+  ALTAR -> [altar]
+  FOUNTAIN -> [fountain]
+  CUBE -> [cube]
+  THRONE -> [throne]
+  BOX -> [box]
 
-drawRoom : Int -> Room -> Room -> [Form]
-drawRoom z r a =
+hdoor = image 56 8 "hdoor.png" |> toForm
+hwall = image 56 8 "hwall.png" |> toForm
+vdoor = image 8 56 "vdoor.png" |> toForm
+vwall = image 8 56 "vwall.png" |> toForm
+
+drawRoom : Room -> [Form]
+drawRoom r =
   (case r.top of
     EMPTY -> []
     DOOR -> [hdoor |> moveY 24]
@@ -120,10 +179,11 @@ drawRoom z r a =
     EMPTY -> []
     DOOR -> [vdoor |> moveX -24]
     WALL -> [vwall |> moveX -24])
-  ++ featureSymbolAt z r.feature a.feature
+  ++ featureSymbol r.feature
 
-drawRoomAt : Coord -> Coord -> Coord -> (Int, Int) -> [Form]
-drawRoomAt x y z (dx, dy) = drawRoom z (roomAt (x + dx) (y + dy) z) (roomAt (x + dx) (y + dy) (z - 1)) |>
+drawRoomAt : Position -> (Int, Int) -> [Form]
+drawRoomAt p (dx, dy) = 
+  drawRoom (roomAt {p | x <- p.x + dx, y <- p.y + dy}) |>
   map (move (48 * toFloat dx, -48 * toFloat dy))
 
 offsets = [-7 .. 7]
@@ -131,11 +191,11 @@ offsets = [-7 .. 7]
 allOffsets : [(Int, Int)]
 allOffsets = concatMap (\y -> map (\ x -> (x, y)) offsets) offsets
 
-drawNeighborhood : Coord -> Coord -> Coord -> [Form]
-drawNeighborhood x y z = concatMap (drawRoomAt x y z) allOffsets
+drawNeighborhood : Position -> [Form]
+drawNeighborhood p = concatMap (drawRoomAt p) allOffsets
 
 drawView : GameState -> Element
-drawView {x, y, z} = (collage 632 632 (drawNeighborhood x y z) |> color black)
+drawView p = (collage 632 632 (drawNeighborhood p) |> color black)
   `beside` container 200 632 midTop [markdown|
 ![](stairs.png) Stairway up
 
@@ -167,39 +227,7 @@ drawView {x, y, z} = (collage 632 632 (drawNeighborhood x y z) |> color black)
 
 |] |> color charcoal
 
-bound : Int -> Int
-bound x = clamp 1 200 x
-
-vbound : Int -> Int
-vbound x = clamp 1 50 x
-
-update : Command -> GameState -> GameState
-update c s = case c of
-  ERROR -> s
-  NORTH -> {s | y <- bound (s.y - 1)}
-  EAST -> {s | x <- bound (s.x + 1)}
-  SOUTH -> {s | y <- bound (s.y + 1)}
-  WEST -> {s | x <- bound (s.x - 1)}
-  UP -> {s | z <- vbound (s.z - 1)}
-  DOWN -> {s | z <- vbound (s.z + 1)}
-
-keyCodeCommand : Keyboard.KeyCode -> Command
-keyCodeCommand n = case n of
-  37 -> WEST
-  38 -> NORTH
-  39 -> EAST
-  40 -> SOUTH
-  68 -> DOWN
-  85 -> UP
-  _ -> ERROR
-
-initState = {x = 25, y = 13, z = 1}
-
-commandStream : Signal Command
-commandStream = keyCodeCommand <~ Keyboard.lastPressed
-
-stateSignal : Signal GameState
-stateSignal = foldp update initState commandStream
+-- Program
 
 main : Signal Element
 main = drawView <~ stateSignal
